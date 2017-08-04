1b75089eb109265c2731b7fd6d7bf327cedae095
emersion <contact@emersion.fr>
2017-08-04 10:22:54 +0200

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ extern crate nom;
 use std::str;
 use nom::{be_u16, le_u8, le_u16, le_u32};
 
-#[derive(Debug)]
-struct Header {
+#[derive(Debug, PartialEq)]
+pub struct Header {
 	vendor: [char; 3],
 	product: u16,
 	serial: u32,
@@ -25,7 +25,7 @@ fn parse_vendor(v: u16) -> [char; 3] {
 	]
 }
 
-named!(header<&[u8], Header>, do_parse!(
+named!(parse_header<&[u8], Header>, do_parse!(
 	tag!(&[0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00][..])
 	>> vendor: be_u16
 	>> product: le_u16
@@ -37,8 +37,8 @@ named!(header<&[u8], Header>, do_parse!(
 	>> (Header{vendor: parse_vendor(vendor), product, serial, week, year, version, revision})
 ));
 
-#[derive(Debug)]
-struct Display {
+#[derive(Debug, PartialEq)]
+pub struct Display {
 	video_input: u8,
 	width: u8, // cm
 	height: u8, // cm
@@ -46,7 +46,7 @@ struct Display {
 	features: u8,
 }
 
-named!(display<&[u8], Display>, do_parse!(
+named!(parse_display<&[u8], Display>, do_parse!(
 	video_input: le_u8
 	>> width: le_u8
 	>> height: le_u8
@@ -55,23 +55,23 @@ named!(display<&[u8], Display>, do_parse!(
 	>> (Display{video_input, width, height, gamma, features})
 ));
 
-named!(chromaticity<&[u8], ()>, do_parse!(
+named!(parse_chromaticity<&[u8], ()>, do_parse!(
 	take!(10) >> ()
 ));
 
-named!(established_timing<&[u8], ()>, do_parse!(
+named!(parse_established_timing<&[u8], ()>, do_parse!(
 	take!(3) >> ()
 ));
 
-named!(standard_timing<&[u8], ()>, do_parse!(
+named!(parse_standard_timing<&[u8], ()>, do_parse!(
 	take!(16) >> ()
 ));
 
 // TODO: code page 437 (https://en.wikipedia.org/wiki/Code_page_437)
-named!(descriptor_text<&[u8], &str>, map!(map_res!(take!(13), str::from_utf8), |s| s.trim()));
+named!(parse_descriptor_text<&[u8], &str>, map!(map_res!(take!(13), str::from_utf8), |s| s.trim()));
 
-#[derive(Debug)]
-enum Descriptor {
+#[derive(Debug, PartialEq)]
+pub enum Descriptor {
 	DetailedTiming, // TODO
 	SerialNumber(String),
 	UnspecifiedText(String),
@@ -79,21 +79,49 @@ enum Descriptor {
 	Name(String),
 	WhitePoint, // TODO
 	StandardTiming, // TODO
-	Unknown,
+	Unknown([u8; 13]),
 }
 
-named!(descriptor<&[u8], Descriptor>,
+named!(parse_descriptor<&[u8], Descriptor>,
 	switch!(le_u16,
 		0 => do_parse!(
 			take!(1)
 			>> d: switch!(le_u8,
-				0xFF => do_parse!(take!(1) >> s: descriptor_text >> (Descriptor::SerialNumber(s.to_string()))) |
-				0xFE => do_parse!(take!(1) >> s: descriptor_text >> (Descriptor::UnspecifiedText(s.to_string()))) |
-				0xFD => do_parse!(take!(1) >> take!(13) >> (Descriptor::RangeLimits)) |
-				0xFC => do_parse!(take!(1) >> s: descriptor_text >> (Descriptor::Name(s.to_string()))) |
-				0xFB => do_parse!(take!(1) >> take!(13) >> (Descriptor::WhitePoint)) |
-				0xFA => do_parse!(take!(1) >> take!(13) >> (Descriptor::StandardTiming)) |
-				_ => do_parse!(take!(1) >> take!(13) >> (Descriptor::Unknown))
+				0xFF => do_parse!(
+					take!(1)
+					>> s: parse_descriptor_text
+					>> (Descriptor::SerialNumber(s.to_string()))
+				) |
+				0xFE => do_parse!(
+					take!(1)
+					>> s: parse_descriptor_text
+					>> (Descriptor::UnspecifiedText(s.to_string()))
+				) |
+				0xFD => do_parse!(
+					take!(1)
+					>> take!(13)
+					>> (Descriptor::RangeLimits)
+				) |
+				0xFC => do_parse!(
+					take!(1)
+					>> s: parse_descriptor_text
+					>> (Descriptor::Name(s.to_string()))
+				) |
+				0xFB => do_parse!(
+					take!(1)
+					>> take!(13)
+					>> (Descriptor::WhitePoint)
+				) |
+				0xFA => do_parse!(
+					take!(1)
+					>> take!(13)
+					>> (Descriptor::StandardTiming)
+				) |
+				_ => do_parse!(
+					take!(1) >>
+					data: count_fixed!(u8, le_u8, 13) >>
+					(Descriptor::Unknown(data))
+				)
 			)
 			>> (d)
 		) |
@@ -101,36 +129,74 @@ named!(descriptor<&[u8], Descriptor>,
 	)
 );
 
-#[derive(Debug)]
-struct EDID {
+#[derive(Debug, PartialEq)]
+pub struct EDID {
 	header: Header,
 	display: Display,
+	chromaticity: (), // TODO
+	established_timing: (), // TODO
+	standard_timing: (), // TODO
 	descriptors: Vec<Descriptor>,
 }
 
-named!(edid<&[u8], EDID>, do_parse!(
-	header: header
-	>> display: display
-	>> chromaticity
-	>> established_timing
-	>> standard_timing
-	>> descriptors: count!(descriptor, 4)
+named!(parse_edid<&[u8], EDID>, do_parse!(
+	header: parse_header
+	>> display: parse_display
+	>> chromaticity: parse_chromaticity
+	>> established_timing: parse_established_timing
+	>> standard_timing: parse_standard_timing
+	>> descriptors: count!(parse_descriptor, 4)
 	>> take!(1) // number of extensions
 	>> take!(1) // checksum
-	>> (EDID{header, display, descriptors})
+	>> (EDID{header, display, chromaticity, established_timing, standard_timing, descriptors})
 ));
+
+pub fn parse(data: &[u8]) -> nom::IResult<&[u8], EDID> {
+	parse_edid(data)
+}
 
 #[cfg(test)]
 mod tests {
 	use super::*;
 
+	fn test(d: &[u8], expected: &EDID) {
+		let (remaining, parsed) = parse(d).unwrap();
+		assert_eq!(remaining.len(), 0);
+		assert_eq!(&parsed, expected);
+	}
+
 	#[test]
-	fn test_host() {
-		//let d = include_bytes!("/sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-LVDS-1/edid");
-		let d = include_bytes!("/sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-VGA-1/edid");
-		println!("{} {:?}", d.len(), &d[..]);
-		let (remaining, result) = edid(d).unwrap();
-		println!("{} {:?}", remaining.len(), remaining);
-		println!("{} {:?}", d.len() - remaining.len(), result);
+	fn test_card0_vga_1() {
+		let d = include_bytes!("../testdata/card0-VGA-1");
+
+		let expected = EDID{
+			header: Header{
+				vendor: ['S', 'A', 'M'],
+				product: 596,
+				serial: 1146106418,
+				week: 27,
+				year: 17,
+				version: 1,
+				revision: 3,
+			},
+			display: Display{
+				video_input: 14,
+				width: 47,
+				height: 30,
+				gamma: 120,
+				features: 42,
+			},
+			chromaticity: (),
+			established_timing: (),
+			standard_timing: (),
+			descriptors: vec!(
+				Descriptor::DetailedTiming,
+				Descriptor::RangeLimits,
+				Descriptor::Name("SyncMaster".to_string()),
+				Descriptor::SerialNumber("HS3P701105".to_string()),
+			),
+		};
+
+		test(d, &expected);
 	}
 }
